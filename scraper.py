2764e8576771da18804d96df8cb4be2f0dc54ced
Ganda Software Factory <ganda.software.factory@gmail.com>
2015-04-15 13:11:45 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,18 +1,24 @@
 # This is a template for a Python scraper on Morph (https://morph.io)
 # including some code snippets below that you should find helpful
 
-import scraperwiki
+# import scraperwiki
 # import lxml.html
+import mechanize
+
+br = mechanize.Browser()
+br.open("http://www.congreso.es/portal/page/portal/Congreso/Congreso/Diputados?_piref73_1333056_73_1333049_1333049.next_page=/wc/menuAbecedarioInicio&tipoBusqueda=completo&idLegislatura=10")
+for link in br.links():
+  print link
 #
 # # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
+# html = scraperwiki.scrape("http://www.example.com/")
 #
 # # Find something on the page using css selectors
 # root = lxml.html.fromstring(html)
 # root.cssselect("div[align='left']")
 #
 # # Write out to the sqlite database using scraperwiki library
-scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 #
 # # An arbitrary query against the database
 # scraperwiki.sql.select("* from data where 'name'='peter'")
